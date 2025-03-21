e87089263fdf26116965e32a4d28504f001c0c89
Gustavo Rocha Aureliano <gurocha27@gmail.com>
2025-03-21 10:30:07 -0300

    modifica frase sobre otimização

--- lerolero.py
+++ lerolero.py
@@ -2,7 +2,7 @@
 Gera frases de efeito sem significado real."""
 import ramdom
 
-parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo otimizado"]
+parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo otimizado, e"]
 parte2 = ["possui exelente desempenho", "oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
 parte3 = ["nas aplicações a que se destina", "em relação às opções disponíveis no mercado", ", provendo ampla vantagem competitiva a seus usuários"]
 
