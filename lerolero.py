45e47455e69948d48643a264a6dbc35904ab1dee
gabriellp <gabriel.prodossimo@usp.br>
2025-03-21 09:57:56 -0300

    Implementa esqueleto do programa.

--- lerolero.py
+++ lerolero.py
@@ -1,2 +1,9 @@
 """Gerador de Lero Lero.
 Gera frases de efeito sem significado real."""
+import ramdom
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print (ramdom.choice(parte1), ramdom.choice(parte2), ramdom.choice(parte3))
