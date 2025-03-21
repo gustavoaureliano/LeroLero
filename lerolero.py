751659def3c913d49fd08dfa3e9750eb73b07303
Gustavo Rocha Aureliano <gurocha27@gmail.com>
2025-03-21 10:13:42 -0300

    Cria o esqueleto da versão multi-línguas

--- lerolero.py
+++ lerolero.py
@@ -6,4 +6,17 @@ parte1 = []
 parte2 = []
 parte3 = []
 
-print (ramdom.choice(parte1), ramdom.choice(parte2), ramdom.choice(parte3))
+lingua = int(input(
+    """Escolha a lingua:
+    [1] - português
+    [2] - inglês
+    :
+    """))
+
+if lingua == 2:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+
+
+print(ramdom.choice(parte1), ramdom.choice(parte2), ramdom.choice(parte3))
